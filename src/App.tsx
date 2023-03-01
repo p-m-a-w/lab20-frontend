a4de2824096bc780ab01a79b0e4e767d7ecef52c
PutawanDE <putawan-de@hotmail.co.th>
2023-03-02 02:42:27 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Painting from "./components/Painting";
+
+function App() {
+  return (
+    <div className="App">
+      <Painting />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Painting.tsx
@@ -0,0 +1,75 @@
+import { Client } from "@stomp/stompjs";
+import React, { useEffect, useState } from "react";
+import Cell from "./Cell";
+
+let client: Client;
+
+let color: string;
+
+function Painting() {
+  const [colorGrid, setColorGrid] = useState([[]]);
+
+  useEffect(() => {
+    if (!client) {
+      color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+      client = new Client({
+        brokerURL: "ws://10.83.234.218:8080/demo-websocket",
+        onConnect: () => {
+        client.subscribe("/app/canvas", (message) => {
+          const body = JSON.parse(message.body);
+          setColorGrid(body["colorGrid"]);
+        });
+
+        client.subscribe("/topic/canvas", (message) => {
+          const body = JSON.parse(message.body);
+          setColorGrid(body["colorGrid"]);
+        });
+      }});
+
+      client.activate();
+    }
+  }, []);
+
+  const paint = (x: number, y: number) => {
+    if (client) {
+      if (client.connected) {
+        client.publish({
+          destination: "/app/paint",
+          body: JSON.stringify({
+            color: color, // random color
+            posX: x,
+            posY: y,
+          }),
+        });
+      }
+    }
+  };
+
+  return (
+    <div>
+      <table
+        style={{
+          borderCollapse: "collapse",
+        }}
+      >
+        <tbody>
+          {colorGrid.map((row, j) => (
+            <tr key={j}>
+              {row.map((col, i) => (
+                <Cell
+                  x={i}
+                  y={j}
+                  key={`${i}${j}`}
+                  paint={paint}
+                  color={colorGrid[j][i]}
+                />
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Painting;
